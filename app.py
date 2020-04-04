5c9305e0d1ffb55666250337a3f3232e86faceda
Patrick Kinsella <pkinsella1@gmail.com>
2020-04-04 16:06:50 -0600

    search form on /index view does nothing

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from forms import SearchForm
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'a-023hr0983iuooianf0987g1'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  form = SearchForm()
+  if form.validate_on_submit():
+      form.artist.data = ''
+      form.term.data = ''
+  return render_template('index.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
